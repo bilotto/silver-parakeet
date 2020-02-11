bf42925f405c745d4f465bcba0eb8655559d104d
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-11 14:23:11 -0300

    Adapting nodeObject script to the NodeNew class

--- vars/nodeObject.groovy
+++ vars/nodeObject.groovy
@@ -1,64 +1,36 @@
-import org.com.Node
-import org.com.Properties
+import org.com.NodeNew
+import org.com.PropertiesNew
 
 def call(String user, String hostname, String base_dir, Node jump_server){
-	Node node = new Node()
-	node.construct(user, hostname, base_dir, jump_server)
+	return new NodeNew(user, hostname, base_dir, jump_server)
+}
+
+Node createNodeObject(String nodeId, PropertiesNew properties){
+	def jpNode = null
+	nodeProperties = properties.getNodeProperties(nodeId)
+	if (nodeProperties.get('JUMP_SERVER')) {
+		jpId = node_properties.get('JUMP_SERVER')
+		jpNode = this.createNodeObject(jpId, properties)
+	}
+	def user = node_properties.get('USER')
+	def hostname = node_properties.get('HOSTNAME')
+	def homeDir = node_properties.get('HOME_DIR')
+	node = this.call(user, hostname, base_dir, jpNode)
+	if (node_properties.get('RELEASE_BASE_DIR')) {
+		node.releaseBaseDir = node_properties.get('RELEASE_BASE_DIR')
+	}
 	return node
 }
 
-def create_node_obj(Map node_properties, Properties properties){
-	jp_node = null
-	if (node_properties.get('JUMP_SERVER')) {
-		jp_id = node_properties.get('JUMP_SERVER')
-		jp_properties = properties.get_node_properties(jp_id)
-		jp_node = this.create_node_obj(jp_properties, properties)
-	}
-	user = node_properties.get('USER')
-	hostname = node_properties.get('HOSTNAME')
-	base_dir = node_properties.get('BASE_DIR')
-	node = this.call(user, hostname, base_dir, jp_node)
-	return node
-}
-
-def create_node_object(node_properties, jump_server_objects){
-	def user = node_properties.USER
-	def hostname = node_properties.HOSTNAME
-	def base_dir = node_properties.BASE_DIR
-	def jp_id = node_properties.JUMP_SERVER
-	if (jp_id) {
-		jump_server = jump_server_objects.get(jp_id)
-	} else {
-		jump_server = null
-	}
-	return this.call(user, hostname, base_dir, jump_server)
-}
-
-
-def createNodeObjectsFromList(properties, nodeListId){
-	nodeIdList = properties.get_list_of_nodes(nodeListId)
-
-	jpObjects = [:]
-	nodeObjects = [:]
-	
-	for (node_id in nodeIdList) {
-		node_properties = properties.get_node_properties(node_id)
-		println node_properties
-		if (node_properties.get('JUMP_SERVER')) {
-			jp_id = node_properties.JUMP_SERVER.toString()
-			jp_properties = properties.get_node_properties(jp_id)
-			if (!jpObjects.get(jp_id)) {
-				jp_node = this.create_node_object(jp_properties, jpObjects)
-				jpObjects.put(jp_id, jp_node)
-			}
-		}
-		println jpObjects
-		node = this.create_node_object(node_properties, jpObjects)
-		nodeObjects.put(node_id, node)
+Map createNodeObjectsFromList(nodeListId, properties){
+	nodeIdList = properties.getNodeList(nodeListId)
+	nodeObjects = [ : ]
+	nodeIdList.each { nodeId ->
+		node = this.createNodeObject(nodeId, properties)
+		nodeObjects.put(nodeId, node)
 	}
 	return nodeObjects
 }
-
 
 
 return this
