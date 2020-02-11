ba06c34bd1c90394799459f682eccdb45252019f
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-11 13:36:05 -0300

    New attributes and methods

--- src/org/com/PropertiesNew.groovy
+++ src/org/com/PropertiesNew.groovy
@@ -2,21 +2,26 @@ package org.com
 
 class PropertiesNew {
 	Map propertiesMap
+	Map NODES
+	Map NODE_LIST
+	Map GIT_PROPERTIES
 	PropertiesNew(propertiesMap) {
 		this.propertiesMap = propertiesMap
+		this.NODES = propertiesMap.NODES
+		this.NODE_LIST = propertiesMap.NODE_LIST
+		this.GIT_PROPERTIES = propertiesMap.GIT_PROPERTIES
 	}
 	
-	
 	Map getGitProperties(String projectName) {
-		if (!propertiesMap.GIT_PROPERTIES.get(projectName)) {
+		if (!this.GIT_PROPERTIES.get(projectName)) {
 			error "Git project properties not found"
 		}
-		return propertiesMap.GIT_PROPERTIES.get(projectName)                               
+		return this.GIT_PROPERTIES.get(projectName)                               
 	}
 	
 	
 	Map getNodeProperties(String nodeId) {
-		for (entry in propertiesMap.NODES) {
+		for (entry in this.NODES) {
 			for (node in entry.value) {
 				if (nodeId == node.key) {
 					return node.value
@@ -24,5 +29,17 @@ class PropertiesNew {
 			}
 		}
 		error "Node ${nodeId} not found"
-	}	
+	}
+	
+	
+	List getNodeList(String nodeListId) {
+		for (entry in this.NODE_LIST) {
+			for (nodeList in entry.value) {
+				if (nodeListId == nodeList.key) {
+					return nodeList.value
+				}
+			}
+		}
+		error "Node list ${nodeListId} not found"
+	}
 }
