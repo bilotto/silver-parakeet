c563229943716656e3dd4ce6bcd05e89e2d90af3
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-12 14:52:00 -0300

    Replacement of Cluster class

--- src/org/com/Cluster.groovy
+++ src/org/com/Cluster.groovy
@@ -1,30 +1,20 @@
 package org.com
 
-
-def print_object(){
-	println clusterEnv
-}
-
-def construct(List node_list, String role){
-	clusterEnv = [
-			'node_list': node_list,
-			'role': role
-	]
-	this.print_object()
-}
-
-def execute(String command){
-	if (clusterEnv.role == "PASSIVE") {
-		branches = [ : ]
-		clusterEnv.node_list.each { node ->
-			branches[ node ] = { node.execute_command(command) }
+class Cluster {
+	String name
+	List nodeList
+	Cluster(name, nodeList) {
+		this.name = name
+		this.nodeList = nodeList
+	}
+	
+	void execute(String command){
+		this.nodeList.each { node ->
+			branches[ node.hostname ] = { node.execute(command) }
 		}
 		parallel branches
-	} else {
-		clusterEnv.node_list.each { node ->
-			node.execute_command(command)
-		}		
 	}
+	
 }
 
 
