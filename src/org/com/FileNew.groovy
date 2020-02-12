152a99d65ef77794414597abe7e09808a683791e
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-12 16:37:10 -0300

    FileNew adding the cksum attribute during constructor

--- src/org/com/FileNew.groovy
+++ src/org/com/FileNew.groovy
@@ -5,12 +5,14 @@ class FileNew {
 	String directory
 	NodeNew node
 	String fullPath
+	String cksum
 	FileNew(name, directory, node) {
 		this.name = name
 		this.node = node
 		this.directory = directory 
 		this.fullPath = "${this.directory}/${this.name}"
+		def cksumCommand = "cd ${this.directory}; cksum ${this.name}"
+		this.cksum = node.executeAndGetOutput(cksumCommand)
 	}
-
 }
 
