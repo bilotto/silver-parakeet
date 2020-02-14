d6b89de3f3e5038a3ada8b586d11b780e11bd4ea
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-14 11:56:19 -0300

    Log script with debug option

--- vars/log.groovy
+++ vars/log.groovy
@@ -2,6 +2,12 @@ def call(String logMessage){
 	println logMessage
 }
 
+def call(String logType, String logMessage){
+	if (env.LOG_TYPE == logType){
+		println logMessage
+	}
+}
+
 def raiseError(String logMessage){
 	error logMessage
 }
