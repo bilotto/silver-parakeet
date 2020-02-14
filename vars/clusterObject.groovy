0b1106f2f26bae437867c384a14e9faedff8a06e
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-14 11:55:10 -0300

    clusterObject script adapted to the new class

--- vars/clusterObject.groovy
+++ vars/clusterObject.groovy
@@ -1,15 +1,8 @@
 import org.com.Cluster
 
-def call(Map node_objects, String role){
-
-	if (role != 'ACTIVE' && role != 'PASSIVE') {
-		error "Invalid cluster role: ${role}"
-	}
-
-	node_list = []
-	node_objects.each{ k, v -> node_list.add(v) }
-
-	Cluster cluster = new Cluster()
-	cluster.construct(node_list, role)
-	return cluster
+def call(String clusterName, Map nodeObjects){
+	nodeList = []
+	nodeObjects.each{ k, v -> nodeList.add(v) }
+	return new Cluster(clusterName, nodeList)
 }
+
