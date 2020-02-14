66b85bcd84212441abfb2172925174017a48adb7
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-14 14:44:41 -0300

    Don't create the Cluster object if nodeList is empty

--- vars/clusterObject.groovy
+++ vars/clusterObject.groovy
@@ -3,6 +3,9 @@ import org.com.Cluster
 def call(String clusterName, Map nodeObjects){
 	nodeList = []
 	nodeObjects.each{ k, v -> nodeList.add(v) }
+	if (!nodeList.size()){
+	    error "empty node list"
+	}
 	return new Cluster(clusterName, nodeList, tools)
 }
 
