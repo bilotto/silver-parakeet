62fa25f8b207f659e13d3105f82afcd0d7c41635
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-14 13:23:34 -0300

    Passing tools to Cluster construct

--- vars/clusterObject.groovy
+++ vars/clusterObject.groovy
@@ -3,6 +3,6 @@ import org.com.Cluster
 def call(String clusterName, Map nodeObjects){
 	nodeList = []
 	nodeObjects.each{ k, v -> nodeList.add(v) }
-	return new Cluster(clusterName, nodeList)
+	return new Cluster(clusterName, nodeList, tools)
 }
 
