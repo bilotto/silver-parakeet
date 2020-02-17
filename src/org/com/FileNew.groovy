5549310f0fd716ace79813f2a673453a2787393d
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-17 10:01:58 -0300

    New File constructor with no node reference

--- src/org/com/FileNew.groovy
+++ src/org/com/FileNew.groovy
@@ -13,33 +13,45 @@ class FileNew {
 		this.fullPath = "${this.directory}/${this.name}"
 	}
 	
-	Boolean existsInNode(NodeNew node, String directory){
-		def command = "cd ${directory}; if [ -e ${this.name} ]; then echo true; else echo false; fi"
-	    def stdout = node.executeAndGetOutput(command)
-		if (stdout != 'true'){
-			println "It does not exist"
-			return false
-		}
-		println "File exists. Is the same file?"
-		def cksumCommand = "cd ${directory}; cksum ${this.name}"
-		def cksum = node.executeAndGetOutput(cksumCommand)
-		if (cksum == this.cksum) {
-			println "They are the same file"
-			return true
-		}
-		println "They are not the same file"
-		return false
-	}
-	
-	void replaceNode(node, directory) {
-		this.node = node
-		this.directory = directory
+	FileNew(name, directory) {
+		this.name = name
+		this.directory = directory 
 		this.fullPath = "${this.directory}/${this.name}"
 	}
 	
+	Boolean existsInNode(NodeNew node, String directory){
+		if (this.node){
+			def command = "cd ${directory}; if [ -e ${this.name} ]; then echo true; else echo false; fi"
+		    def stdout = node.executeAndGetOutput(command)
+			if (stdout != 'true'){
+				println "It does not exist"
+				return false
+			}
+			println "File exists. Is the same file?"
+			def cksumCommand = "cd ${directory}; cksum ${this.name}"
+			def cksum = node.executeAndGetOutput(cksumCommand)
+			if (cksum == this.cksum) {
+				println "They are the same file"
+				return true
+			}
+			println "They are not the same file"
+			return false
+		}
+	}
+	
+	void replaceNode(node, directory) {
+		if (this.node){
+			this.node = node
+			this.directory = directory
+			this.fullPath = "${this.directory}/${this.name}"
+		}
+	}
+	
 	void deleteItself(){
-		def command = "rm -f ${this.fullPath}"
-		this.node.execute(command)  
+		if (this.node){
+			def command = "rm -f ${this.fullPath}"
+			this.node.execute(command)
+		}
 	}
 	
 }
