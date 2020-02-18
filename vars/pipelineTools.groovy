093324538ff26a3c375a24736069da30dfa68cd5
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-18 09:37:32 -0300

    New pipelineTools

--- vars/pipelineTools.groovy
+++ vars/pipelineTools.groovy
@@ -1,11 +1,5 @@
-/** A map that holds all constants and data members that can be override when constructing  */
-pipelineEnv
-
-def construct(){
-	pipelineEnv = [
-			tools : tools,
-			log: log,
-			regex: regex
-	]
+def call(){
+	def pipelineEnv = [ : ]
+	pipelineEnv.tools = tools
+	return pipelineEnv
 }
-
