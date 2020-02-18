4f1811b40a5823de8443429173d53deeea9e9697
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-18 19:32:05 -0300

    log script improvement

--- vars/log.groovy
+++ vars/log.groovy
@@ -3,20 +3,32 @@ def call(String logMessage){
 }
 
 def call(String logType, String logMessage){
+	logMessage = "${logType}: ${logMessage}"
 	if (logType == "DEBUG") {
-		if (env.DEBUG == "true") {
-			println logMessage                  
+		if (env.DEBUG) {
+			if (env.DEBUG == "true") {
+				println logMessage                  
+			}    
+		}
+	}
+	if (logType == "INFO") {
+		if (env.INFO) {
+			if (env.INFO == "true") {
+				println logMessage                  
+			}    
 		}
 	}
 	if (logType == "ERROR") {
-		if (env.ERROR == "true") {
-			println logMessage                  
+		if (env.ERROR) {
+			if (env.ERROR == "true") {
+				println logMessage                  
+			}    
 		}
 	}
 }
 
 def raiseError(String logMessage){
+	this.call("ERROR", logMessage)
 	error logMessage
 }
 
-return this
