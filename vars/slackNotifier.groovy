943a463d098e4ada3105cde2740b9ac065e4bd02
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-19 09:51:37 -0300

    New method stackSlackMessage

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -19,10 +19,16 @@ def bkp(String buildResult, String channel) {
   }
 }
 
-def stackSlackMessage(message){
-  if (!env.SLACK_MESSAGE) {
+def stackSlackMessage_bkp(message){
+  if (env.SLACK_MESSAGE) {
     env.SLACK_MESSAGE = message
   } else {
     env.SLACK_MESSAGE = env.SLACK_MESSAGE + "\n" + message
   }
+}
+
+def stackSlackMessage(message){
+	if (env.SLACK_MESSAGE) {
+    	env.SLACK_MESSAGE = env.SLACK_MESSAGE + message
+    }
 }
