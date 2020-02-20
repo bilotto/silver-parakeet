a950421f03f829d6838f7c57d5560da676b90d75
Fabio Bilotto <fabio.bilotto@openet.com>
2020-02-20 09:43:41 -0300

    New script Bash

---
+++ vars/bash.groovy
@@ -0,0 +1,38 @@
+def makeSshCommand(remoteUser, remoteHostname, remoteCommand){
+	remoteCommand = this.stringToCommand(remoteCommand)
+	def cmd = """
+				ssh ${remoteUser}@${remoteHostname} \
+				\"
+					${remoteCommand}
+				\"
+			"""
+	return cmd   
+}
+
+def executeLocalCommand(command, returnOutput){
+	if (returnOutput) {
+	       stdout = sh (returnStdout: true, script: "${cmd}").trim().toString()
+	       return stdout
+	} else {
+	      sh "${cmd}"
+  	}
+}
+
+def executeRemoteCommand(String remoteUser, String remoteHostname, String remoteCommand, Boolean returnOutput) {
+	def command = this.makeSshCommand(remoteUser, remoteHostname, remoteCommand)
+	this.executeLocalCommand(command, returnOutput)
+}
+
+
+def executeRemoteCommandThroughJumpServer(jumpServerUser, jumpServerHostname, String remoteUser, String remoteHostname, String remoteCommand, Boolean returnOutput) {
+	remoteCommand = this.makeSshCommand(remoteUser, remoteHostname, remoteCommand)
+	def command = """
+				ssh ${jumpServerUser}@${jumpServerHostname} \
+				'
+					${remoteCommand}
+				'
+			"""
+	this.executeLocalCommand(command, returnOutput)
+	
+			
+}
